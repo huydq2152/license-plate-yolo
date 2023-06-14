99454d287be03188282e0cecd06679c8af80fd8f
0377581629 <quanghuy2152@gmail.com>
2023-06-14 15:23:45 +0700

    update code

---
+++ static/extract_text_cnn/data_utils.py
@@ -0,0 +1,69 @@
+import numpy as np
+import cv2
+
+
+def get_digits_data(path):
+    data = np.load(path, allow_pickle=True)
+    total_nb_data = len(data)
+    np.random.shuffle(data)
+    data_train = []
+
+    for i in range(total_nb_data):
+        data_train.append(data[i])
+
+    print("-------------DONE------------")
+    print('The number of train digits data: ', len(data_train))
+
+    return data_train
+
+
+def get_alphas_data(path):
+    data = np.load(path, allow_pickle=True)
+    total_nb_data = len(data)
+
+    np.random.shuffle(data)
+    data_train = []
+
+    for i in range(total_nb_data):
+        data_train.append(data[i])
+
+    print("-------------DONE------------")
+    print('The number of train alphas data: ', len(data_train))
+
+    return data_train
+
+def convert2Square(image):
+    """
+    Resize non square image(height != width to square one (height == width)
+    :param image: input images
+    :return: numpy array
+    """
+
+    img_h = image.shape[0]
+    img_w = image.shape[1]
+
+    # if height > width
+    if img_h > img_w:
+        diff = img_h - img_w
+        if diff % 2 == 0:
+            x1 = np.zeros(shape=(img_h, diff//2))
+            x2 = x1
+        else:
+            x1 = np.zeros(shape=(img_h, diff//2))
+            x2 = np.zeros(shape=(img_h, (diff//2) + 1))
+
+        squared_image = np.concatenate((x1, image, x2), axis=1)
+    elif img_w > img_h:
+        diff = img_w - img_h
+        if diff % 2 == 0:
+            x1 = np.zeros(shape=(diff//2, img_w))
+            x2 = x1
+        else:
+            x1 = np.zeros(shape=(diff//2, img_w))
+            x2 = x1
+
+        squared_image = np.concatenate((x1, image, x2), axis=0)
+    else:
+        squared_image = image
+
+    return squared_image
